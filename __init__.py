f8fe40bc01f54e4e57d7d1b526d7c4259d019c3f
Sarrion <sarrionmas.a@gmail.com>
2020-03-10 12:18:32 +0000

    simple tree function added

--- __init__.py
+++ __init__.py
@@ -26,8 +26,24 @@ def matrixprint(inputList, ncol, nrow, file):
             stringToPrint += columnList[j][i].ljust(maxLengths[j], " ") + " "
         print(stringToPrint, file = file)
 
+        
 def dirtoni(input_obj):
     if isinstance(input_obj, list):
         return [x for x in input_obj if x[0] != '_']
     else:
         return [x for x in dir(input_obj) if x[0] != '_']
+    
+    
+def tree(obj, depth=False):
+    try:
+        print(obj.__name__)
+    except:
+        print('{}'.format(obj))
+    
+    for method in dirtoni(obj):
+        print('    {}'.format(method))
+        for submethod in dirtoni(eval('obj.{}'.format(method))):
+            print('        {}'.format(submethod))
+            if depth:
+                for subsubmethod in dirtoni(eval('obj.{}.{}'.format(method, submethod))):
+                    print('            {}'.format(subsubmethod))
